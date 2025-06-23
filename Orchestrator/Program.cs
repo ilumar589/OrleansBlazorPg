3900d90d654f9fb648edefaa52e903ad7d36068b
Eduard Parvu <eduard.parvu@rinftech.com>
2025-06-23 16:08:36 +0300

    project structure

--- Orchestrator/Program.cs
+++ Orchestrator/Program.cs
@@ -1,3 +1,18 @@
 var builder = DistributedApplication.CreateBuilder(args);
 
+// Add the resources which you will use for Orleans clustering and
+// grain state storage.
+var storage = builder.AddAzureStorage("storage").RunAsEmulator();
+var clusteringTable = storage.AddTables("clustering");
+var grainStorage = storage.AddBlobs("grain-state");
+
+// Add the Orleans resource to the Aspire DistributedApplication
+// builder, then configure it with Azure Table Storage for clustering
+// and Azure Blob Storage for grain storage.
+var orleans = builder.AddOrleans("default")
+                     .WithClustering(clusteringTable)
+                     .WithGrainStorage("Default", grainStorage);
+
+builder.AddProject<Projects.OrleansServer>("orleansserver");
+
 builder.Build().Run();
---
+++ OrleansContracts/ICounterGrain.cs
@@ -0,0 +1,12 @@
+﻿using Orleans;
+
+namespace OrleansContracts;
+
+[Alias("OrleansContracts.ICounterGrain")]
+public interface ICounterGrain : IGrainWithStringKey
+{
+    [Alias("Get")]
+    ValueTask<int> Get();
+    [Alias("Increment")]
+    ValueTask<int> Increment();
+}
